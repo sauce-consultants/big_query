756a93ec60b7e8a7f446d4c9f798c222cb12b02b
John Polling <john@theled.co.uk>
2017-06-08 10:58:45 +0100

    Feat (list rows): add in ability to get data out of tables

--- lib/big_query/tabledata.ex
+++ lib/big_query/tabledata.ex
@@ -7,6 +7,7 @@ defmodule BigQuery.Tabledata do
   @type insert_row :: {unique_row_id :: String.t, row :: map()}
   @type insert_error :: %{index: non_neg_integer, errors: [Error.t]}
   @type insert_all_response :: %{kind: String.t, insertErrors: [insert_error]}
+  @type list_table_response :: %{kind: String.t, insertErrors: [insert_error]}
 
   @doc """
   Stream data into BigQuery using the Streaming API. Read the BigQuery API documentation [here](https://cloud.google.com/bigquery/docs/reference/v2/tabledata/insertAll) for more information.
@@ -15,7 +16,7 @@ defmodule BigQuery.Tabledata do
   """
   @spec insert_all(String.t, String.t, String.t, [insert_row], [skipInvalidRows: boolean, ignoreUnknownValues: boolean]) :: {:ok, insert_all_response} | {:error, BigQuery.Resource.response | String.t}
   def insert_all(project_id, dataset_id, table_id, rows, opts \\ [skipInvalidRows: false, ignoreUnknownValues: false]) do
-    url = insert_url(project_id, dataset_id, table_id)
+    url = table_action_url(project_id, dataset_id, table_id, "insertAll")
 
     prepared_rows = Enum.into(rows, [], fn ({unique_row_id, row}) ->
       %{
@@ -41,8 +42,22 @@ defmodule BigQuery.Tabledata do
     end
   end
 
-  @spec insert_url(String.t, String.t, String.t) :: String.t
-  defp insert_url(project_id, dataset_id, table_name) do
-    "#{base_url()}/projects/#{project_id}/datasets/#{dataset_id}/tables/#{table_name}/insertAll"
+  @spec table_action_url(String.t, String.t, String.t, String.t) :: String.t
+  defp table_action_url(project_id, dataset_id, table_name, action) do
+    "#{base_url()}/projects/#{project_id}/datasets/#{dataset_id}/tables/#{table_name}/#{action}"
+  end
+
+  @spec list(String.t, String.t, String.t, Tuple.t) :: {:ok, list_table_response} | {:error, String.t}
+  def list(project_id, dataset_id, table_id, options \\ {}) do
+    url = table_action_url(project_id, dataset_id, table_id, "data")
+
+    case get(url) do
+      {:ok, %{status_code: status} = resp} when status in 200..299 ->
+        list_table_response = Poison.decode!(resp[:body])
+        {:ok, list_table_response}
+      {:ok, resp} -> {:error, resp}
+      {:error, reason} ->
+        {:error, "Error listing rows: #{inspect reason}"}
+    end
   end
 end
